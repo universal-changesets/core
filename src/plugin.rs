34ed720c7b3b1cbd8bd04a5db27ffd01e0ae8cca
Alex Way <alexwa@softcat.com>
2024-11-03 15:47:29 +0000

    feat: make more secure by making read only when getting current version

--- src/plugin.rs
+++ src/plugin.rs
@@ -3,24 +3,28 @@ use extism::*;
 use extism_convert::Json;
 use semver::Version;
 
-pub fn setup_plugin() -> anyhow::Result<Plugin> {
+pub fn setup_plugin(read_only: bool) -> anyhow::Result<Plugin> {
     let config = get_config()?;
     let cached_plugin_path = config.cache_plugin_from_url()?;
 
     let plugin_file = Wasm::file(cached_plugin_path);
 
-    let current_dir = std::env::current_dir()?;
+    let mut current_dir = std::env::current_dir()?.to_str().unwrap().to_string();
+
+    if read_only {
+        current_dir = format!("ro:{}", current_dir);
+    }
 
     let manifest = extism::Manifest::new([plugin_file])
         // Mounting to the root as the plugin is expected to be in the root of the fs
-        .with_allowed_path(current_dir.to_str().unwrap().to_string(), "/");
+        .with_allowed_path(current_dir, "/");
 
     let plugin = extism::Plugin::new(manifest, [], true).unwrap();
     return Ok(plugin);
 }
 
 pub fn get_version_via_plugin() -> anyhow::Result<Version> {
-    let mut plugin = setup_plugin()?;
+    let mut plugin = setup_plugin(true)?;
     let response = plugin.call::<&str, &str>("get_version", "")?;
 
     let parsed_version = Version::parse(response.to_string().as_str())?;
@@ -34,7 +38,7 @@ struct SetVersionRequest {
 }
 
 pub fn set_version_via_plugin(version: &Version) -> anyhow::Result<()> {
-    let mut plugin = setup_plugin()?;
+    let mut plugin = setup_plugin(false)?;
 
     let request = SetVersionRequest {
         version: version.to_string(),
